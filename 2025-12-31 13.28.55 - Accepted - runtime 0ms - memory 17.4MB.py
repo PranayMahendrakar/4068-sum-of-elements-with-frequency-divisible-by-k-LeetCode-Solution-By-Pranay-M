4ab1bf7f8677828653be6e52999ed53197f12973
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-21 12:46:34 +0530

    Initial commit

---
+++ 2025-12-31 13.28.55 - Accepted - runtime 0ms - memory 17.4MB.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumDivisibleByK(self, nums: List[int], k: int) -> int:
+        from collections import Counter
+        freq = Counter(nums)
+        total = 0
+        for num, count in freq.items():
+            if count % k == 0:
+                total += num * count
+        return total
